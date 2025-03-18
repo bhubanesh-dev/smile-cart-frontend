6309baac919c0b99102c1045b871284e01f6e100
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-03-18 08:25:04 +0530

    Added submission handler to forms

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,23 +1,51 @@
+import {
+  keysToCamelCase,
+  serializeKeysToSnakeCase,
+} from "@bigbinary/neeto-cist";
 import axios from "axios";
-import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { t } from "i18next";
+import { Toastr } from "neetoui";
 import { evolve } from "ramda";
+
+const shouldShowToastr = response =>
+  typeof response === "object" && response?.noticeCode;
+
+const showSuccessToastr = response => {
+  if (shouldShowToastr(response.data)) Toastr.success(response.data);
+};
+
+const showErrorToastr = error => {
+  if (error.message === t("error.networkError")) {
+    Toastr.error(t("error.noInternetConnection"));
+  } else if (error.response?.status !== 404) {
+    Toastr.error(error);
+  }
+};
 
 const transformResponseKeysToCamelCase = response => {
   if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const responseInterceptors = () => {
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+      showSuccessToastr(response);
+
+      return response.data;
+    },
+    error => {
+      showErrorToastr(error);
+
+      return Promise.reject(error);
+    }
+  );
 };
 
 const requestInterceptors = () => {
   axios.interceptors.request.use(
     evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
   );
-};
-
-const responseInterceptors = () => {
-  axios.interceptors.response.use(response => {
-    transformResponseKeysToCamelCase(response);
-
-    return response.data;
-  });
 };
 
 const setHttpHeaders = () => {
@@ -31,6 +59,6 @@ export default function initializeAxios() {
   axios.defaults.baseURL =
     "https://smile-cart-backend-staging.neetodeployapp.com/";
   setHttpHeaders();
-  requestInterceptors();
   responseInterceptors();
+  requestInterceptors();
 }
